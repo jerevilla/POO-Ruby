2bda52dc0968bfb7786bc6d344bdd00019445533
Synopsis.Revilla Rodriguez <jrevillar@proveedor.intercorp.com.pe>
2019-01-29 17:36:23 -0500

    iEsto es un comentario puts "Hola Mundo" puts 'Hola Mundo'
    puts "Entro y saludo: 'Hola'"
    puts 'Entro y saludo: \'Hola\''
    puts
    entero = 1980
    puts entero
    puts "He nacido en #{entero}."
    puts entero.class
    puts
    real = 2.1
    puts real
    puts
    puts suma = 5 + 4.0
    puts resta = 3 - 6
    puts multi = 9 * 4
    puts divi_entera = 9 / 4
    puts divi_float = 9.0 / 4
    puts resto = 9 % 4

---
+++ ruby_basico/1_tipos_operaciones.rb
@@ -0,0 +1,25 @@
+#Esto es un comentario
+puts "Hola Mundo"
+puts 'Hola Mundo'
+
+puts "Entro y saludo: 'Hola'"
+puts 'Entro y saludo: \'Hola\''
+puts
+#Ejemplo de entero
+entero = 1980
+puts entero
+#Interpolación de variables dentro de una cadena
+puts "He nacido en #{entero}."
+puts entero.class
+puts
+#Ejemplo de Float
+real = 2.1
+puts real
+puts 
+#operaciones
+puts suma = 5 + 4.0
+puts resta = 3 - 6
+puts multi = 9 * 4
+puts divi_entera = 9 / 4 
+puts divi_float = 9.0 / 4
+puts resto = 9 % 4 
